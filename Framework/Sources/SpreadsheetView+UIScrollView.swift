0766d9a247d93596f304d4dbe808da1dea4e26d0
Oraz Atakishiyev <orazz.tm@gmail.com>
2017-07-15 14:27:36 +0500

    Fix an issue that an app crashes if spreadsheet view with UINavigationController on iOS 11

--- Framework/Sources/SpreadsheetView+UIScrollView.swift
+++ Framework/Sources/SpreadsheetView+UIScrollView.swift
@@ -10,7 +10,11 @@ import UIKit
 
 extension SpreadsheetView {
     public override func isKind(of aClass: AnyClass) -> Bool {
-        return rootView.isKind(of: aClass)
+        if #available(iOS 11.0, *) {
+            return super.isKind(of: aClass)
+        } else {
+            return rootView.isKind(of: aClass)
+        }
     }
 
     public var contentOffset: CGPoint {
